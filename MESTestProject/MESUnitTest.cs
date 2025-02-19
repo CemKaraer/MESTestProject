adca3ccbd3e085b7736e1629afaa362dc9bddc68
Cem Karaer <prgu00376@pargesoft.com>
2025-02-19 12:30:18 +0300

    Add project files.

---
+++ MESTestProject/MESUnitTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MESTestProject
+{
+    [TestClass]
+    public class MESUnitTest
+    {
+        [TestMethod]
+        public async Task TestPostProductionReturn()
+        {
+            await HttpPost("https://mtnav.guzelenerji.com.tr/api/MESApi/PostProductionReturn", FileContent("PostProductionReturn.json"));
+        }
+
+        [TestMethod]
+        public async Task TestPostProductionWarehousing()
+        {
+            await HttpPost("https://mtnav.guzelenerji.com.tr/api/MESApi/PostProductionWarehousing", FileContent("PostProductionWarehousing.json"));
+        }
+
+        [TestMethod]
+        public async Task TestGetWorkOrderInformation()
+        {
+            await HttpGet("https://mtnav.guzelenerji.com.tr/api/MESApi/GetWorkOrderInformation", FileContent("Parameter.json"));
+        }
+
+        [TestMethod]
+        public async Task TestGetWorkOrderBOMInformation()
+        {
+            await HttpGet("https://mtnav.guzelenerji.com.tr/api/MESApi/GetWorkOrderBOMInformation", FileContent("Parameter.json"));
+        }
+
+        [TestMethod]
+        public async Task TestGetMaterialPickingList()
+        {
+            await HttpGet("https://mtnav.guzelenerji.com.tr/api/MESApi/GetMaterialPickingList", FileContent("Parameter.json"));
+        }
+
+        [TestMethod]
+        public async Task GetMaterialPickingDetail()
+        {
+            await HttpGet("https://mtnav.guzelenerji.com.tr/api/MESApi/GetMaterialPickingDetail", FileContent("Parameter.json"));
+        }
+
+        private static async Task<string> HttpPost(string url, string jsonContent)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("MES-Header", FileContent("Header.json"));
+                return await (await httpClient.PostAsync(url, new StringContent(jsonContent, Encoding.UTF8, "application/json"))).Content.ReadAsStringAsync();
+            }
+        }
+
+        private static async Task<string> HttpGet(string url, string inputJson)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("MES-Header", FileContent("Header.json"));
+                return await (await httpClient.GetAsync($"{url}?input={Uri.EscapeDataString(inputJson)}")).Content.ReadAsStringAsync();
+            }
+        }
+
+        private static string FileContent(string fileName)
+        {
+            return File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestJson", fileName));
+        }
+    }
+}
---
+++ MESTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MESTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MESTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7077b34e-ff3f-4720-a4b7-59912a0839b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
