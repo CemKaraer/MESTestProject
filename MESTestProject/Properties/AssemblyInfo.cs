2cbdd827e0ada8091391e826f0f9c3e248708a32
Cem Karaer <prgu00376@pargesoft.com>
2025-02-25 11:03:30 +0300

    2025.02.25-11.03

--- MESTestProject/Properties/AssemblyInfo.cs
+++ MESTestProject/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MESTestProject")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright Pargesoft © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
