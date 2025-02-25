4d87b4ca6f1a713bacdc7b62ab002662a4a2c9cc
Cem Karaer <prgu00376@pargesoft.com>
2025-02-25 10:50:24 +0300

    2025.02.24-19.34

--- MESTestProject/MESUnitTest.cs
+++ MESTestProject/MESUnitTest.cs
@@ -10,57 +10,74 @@ namespace MESTestProject
     [TestClass]
     public class MESUnitTest
     {
+        private readonly string baseUri = "https://mtnav.guzelenerji.com.tr/api/MES";
+        // private readonly string baseUri = "http://localhost/dwi_clone/api/MES";
+
         [TestMethod]
         public async Task TestPostProductionReturn()
         {
-            await HttpPost("https://mtnav.guzelenerji.com.tr/api/MESApi/PostProductionReturn", FileContent("PostProductionReturn.json"));
+            var result = await HttpPost(baseUri + "/PostProductionReturn", FileContent("PostProductionReturn.json"));
         }
 
         [TestMethod]
         public async Task TestPostProductionWarehousing()
         {
-            await HttpPost("https://mtnav.guzelenerji.com.tr/api/MESApi/PostProductionWarehousing", FileContent("PostProductionWarehousing.json"));
+            var result = await HttpPost(baseUri + "/PostProductionWarehousing", FileContent("PostProductionWarehousing.json"));
         }
 
         [TestMethod]
         public async Task TestGetWorkOrderInformation()
         {
-            await HttpGet("https://mtnav.guzelenerji.com.tr/api/MESApi/GetWorkOrderInformation", FileContent("Parameter.json"));
+            var result = await HttpGet(baseUri + "/GetWorkOrderInformation");
         }
 
         [TestMethod]
         public async Task TestGetWorkOrderBOMInformation()
         {
-            await HttpGet("https://mtnav.guzelenerji.com.tr/api/MESApi/GetWorkOrderBOMInformation", FileContent("Parameter.json"));
+            var result = await HttpGet(baseUri + "/GetWorkOrderBOMInformation");
         }
 
         [TestMethod]
         public async Task TestGetMaterialPickingList()
         {
-            await HttpGet("https://mtnav.guzelenerji.com.tr/api/MESApi/GetMaterialPickingList", FileContent("Parameter.json"));
+            var result = await HttpGet(baseUri + "/GetMaterialPickingList");
         }
 
         [TestMethod]
         public async Task GetMaterialPickingDetail()
         {
-            await HttpGet("https://mtnav.guzelenerji.com.tr/api/MESApi/GetMaterialPickingDetail", FileContent("Parameter.json"));
+            var result = await HttpGet(baseUri + "/GetMaterialPickingDetail");
         }
 
         private static async Task<string> HttpPost(string url, string jsonContent)
         {
-            using (var httpClient = new HttpClient())
+            try
             {
-                httpClient.DefaultRequestHeaders.Add("MES-Header", FileContent("Header.json"));
-                return await (await httpClient.PostAsync(url, new StringContent(jsonContent, Encoding.UTF8, "application/json"))).Content.ReadAsStringAsync();
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Add("MES-Header", FileContent("Header.json"));
+                    return await (await httpClient.PostAsync(url, new StringContent(jsonContent, Encoding.UTF8, "application/json"))).Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception exc)
+            {
+                throw;
             }
         }
 
-        private static async Task<string> HttpGet(string url, string inputJson)
+        private static async Task<string> HttpGet(string url)
         {
-            using (var httpClient = new HttpClient())
+            try
             {
-                httpClient.DefaultRequestHeaders.Add("MES-Header", FileContent("Header.json"));
-                return await (await httpClient.GetAsync($"{url}?input={Uri.EscapeDataString(inputJson)}")).Content.ReadAsStringAsync();
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Add("MES-Header", FileContent("Header.json"));
+                    return await (await httpClient.GetAsync($"{url}?input={Uri.EscapeDataString(FileContent("Parameter.json"))}")).Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception exc)
+            {
+                throw;
             }
         }
 
